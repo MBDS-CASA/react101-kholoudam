1164692c81684d364ac3070acc6073808d5b3ae0
kholoud <kholoudallam12@gmail.com>
2025-12-22 15:12:59 -0800

    Td01 : ajouter un composant header dans la page

--- src/App.jsx
+++ src/App.jsx
@@ -3,14 +3,27 @@ import Emsi from './assets/logo_emsi.png'
 import UCA from './assets/LOGO_UCA.jpg'
 import './App.css'
 
+function Header()
+{
+  return (
+    <>
+      <div>
+        <img src={Emsi} className="logo Emsi" alt="Emsi" />
+        <img src={UCA} className="logo UCA" alt="UCA" />
+      </div>
+      <h1>Introduction à React</h1>
+      <h4>A la découverte des premières notion de React</h4></>
+  );
+}
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
+    <Header />
       <div>
-          <img src={Emsi} className="logo Emsi" alt="Emsi" />
-          <img src={UCA} className="logo UCA" alt="UCA" />
+        <img src={Emsi} className="logo Emsi" alt="Emsi" />
+        <img src={UCA} className="logo UCA" alt="UCA" />
       </div>
       <h1>Introduction à React</h1>
       <h4>A la découverte des premières notion de React</h4>
